c2458f8e2ccb4f90668dfdaccbecdf4a33260d7b
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2024-10-17 12:39:43 +0200

    updated dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,1 @@
-((nil . (
-         (compile-command . "make -C ../build -j 4")
-         (projectile-project-compilation-cmd . "make -C ../build -j 4")
-         (eval . (setq-local flycheck-gfortran-args
-                             (list (concat "-J" (projectile-project-root) (file-name-as-directory "build/") (file-relative-name (file-name-directory buffer-file-name) (projectile-project-root))))))
-)))
+((nil . ((compile-command . (format "make -C %s -j 4" (concat (locate-dominating-file (buffer-file-name) ".dir-locals.el") "build"))))))
