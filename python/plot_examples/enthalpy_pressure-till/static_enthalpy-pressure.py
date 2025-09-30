79e610880b71bf45724dc3674de5335078070fea
Alberto Otero de la Roza <aoterodelaroza@gmail.com>
2025-09-30 16:08:59 +0200

    reorganized python plot examples

---
+++ python/plot_examples/enthalpy_pressure-till/static_enthalpy-pressure.py
@@ -0,0 +1,18 @@
+## append the gibbs2 python module to the path
+import sys
+sys.path.append('/home/alberto/git/gibbs2/python')
+
+##
+import gibbs2 as g2
+import matplotlib.pyplot as plt
+
+phlist = g2.read_staticphases_from_eos_file("till.eos_static")
+
+fig,ax = plt.subplots()
+fig,ax = g2.plot_enthalpy_pressure(fig,ax,phlist,steprefine=10)
+
+ax.set_xlim([0,30])
+
+fig.tight_layout()
+fig.savefig("enthalpy_pressure.pdf")
+
---
+++ python/plot_examples/phase_diagram-bpheno/phase_diagram.py
@@ -0,0 +1,15 @@
+## append the gibbs2 python module to the path
+import sys
+sys.path.append('/home/alberto/git/gibbs2/python')
+
+##
+import gibbs2 as g2
+import matplotlib.pyplot as plt
+
+phlist = g2.read_phases_from_eos_file("bpheno.eos")
+fig,ax = plt.subplots()
+
+fig,ax = g2.plot_phase_diagram(fig,ax,phlist,steprefine=10)
+
+fig.tight_layout()
+fig.savefig("phase_diagram.png")
